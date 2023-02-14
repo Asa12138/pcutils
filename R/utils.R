7d3229252fa8a5268e372b172470ebbd2faefe67
Asa12138 <3180105399@zju.edu.cn>
2023-02-14 20:03:50 +0800

    update

--- R/utils.R
+++ R/utils.R
@@ -4,17 +4,26 @@
 #     if (!requireNamespace(p)) {
 #            install.packages(p)}}
 
+
 #' Print with =
+#'
 #' @param str output strings
-#' @param n the number of = side chars
+#' @param char side chars default:=
+#' @param n the number of output length
+#' @param mode "middle", "left" or "right"
+#'
 #' @export
-dabiao<-function(str,n=80){
+dabiao<-function(str,n=80,char="=",mode=c("middle", "left", "right")){
+  mode=match.arg(mode,c("middle", "left", "right"))
   if(n<nchar(str))n=nchar(str)+2
   x=(n-nchar(str))%/%2
-  xx=paste0(paste(rep('=',x),collapse = ""),str,paste(rep('=',x),collapse = ""))
+  switch (mode,
+          "left" = {xx=paste0(str,strrep(char,2*x))},
+          "middle" = {xx=paste0(strrep(char,x),str,strrep(char,x))},
+          "right" = {xx=paste0(strrep(char,2*x),str)}
+  )
   cat(xx,"\n")
 }
-
 
 #' Library packages or install
 #'
@@ -52,11 +61,27 @@ lib_ps<-function(p_list,...){
   }
 }
 
+#' Detach packages
+#'
+#' @param p_list a vector of packages list
+#' @export
+del_ps<-function(p_list,...){
+  p_list=c(p_list,...)
+  p_list=paste0("package:",p_list)
+  all=search()
+  p_list=p_list[p_list%in%all]
+  for (p in p_list){
+    detach(p,character.only = T)
+  }
+}
 
 #' @title Min_Max scale
+#'
 #' @param x a numeric vector
 #' @param min_s scale min
 #' @param max_s scale max
+#' @param n linear transfer for n=1; the slope will change if n>1 or n<1
+#' @param plot whether plot the transfer?
 #'
 #' @return a numeric vector
 #' @export
@@ -64,9 +89,13 @@ lib_ps<-function(p_list,...){
 #' @examples
 #' x=runif(10)
 #' mmscale(x,5,10)
-mmscale<-function(x,min_s=0,max_s=1){
+mmscale<-function(x,min_s=0,max_s=1,n=1,plot=F){
+  if(n<=0)stop("n should bigger than 0")
   if((max(x)-min(x))==0)return(rep((min_s+max_s)/2,length(x)))
-  min_s+(x-min(x))/(max(x)-min(x))*(max_s-min_s)
+  x2=((x-min(x))^n)
+  y=min_s+(x2)/(max(x2))*(max_s-min_s)
+  if(plot)plot(y~x)
+  y
 }
 
 #' @title Three-line table
@@ -75,7 +104,7 @@ mmscale<-function(x,min_s=0,max_s=1){
 #' @param digits how many digits should remain
 #' @param nrow show how many rows
 #' @param ncol show how many columns
-#' @param ... additional arguments e.g.(rowname=NULL)
+#' @param ... additional arguments e.g.(rows=NULL)
 #'
 #' @import ggpubr dplyr
 #' @return a ggplot
@@ -84,6 +113,7 @@ mmscale<-function(x,min_s=0,max_s=1){
 #' @examples
 #' data("otutab")
 #' sanxian(otutab)
+#' sanxian(otutab,ncol=4,rows=NULL)
 sanxian<-function(aa,digits = 3,nrow=10,ncol=10,...){
   lib_ps("ggpubr","dplyr")
   if(nrow(aa)>nrow)aa<-aa[1:nrow,,drop=F]
@@ -95,7 +125,6 @@ sanxian<-function(aa,digits = 3,nrow=10,ncol=10,...){
   return(p)
 }
 
-
 #' Transform a rgb vector to a Rcolor code
 #'
 #' @param x vector or three columns data.frame
@@ -105,17 +134,26 @@ sanxian<-function(aa,digits = 3,nrow=10,ncol=10,...){
 #'
 #' @examples
 #' rgb2code(c(12,23,34))
-rgb2code<-function(x){
-  library(dplyr)
-  if(length(x)!=3)stop("need r,g,b!")
-  names(x)=c("r","g","b")
-  if(is.vector(x))return(rgb(x[1],x[2],x[3],maxColorValue = 255))
-  if(is.data.frame(x))return(transmute(x,code=rgb(r,g,b,maxColorValue = 255)))
+#' rgb2code("#69C404",rev=T)
+rgb2code<-function(x,rev=F){
+  lib_ps("dplyr")
+  if(rev){
+    if(is.vector(x))col2rgb(x)%>%t()%>%as.vector()->A;names(A)=c("r","g","b");return(A)
+    if(is.data.frame(x))apply(x, 1,col2rgb)%>%t()->A;colnames(A)=c("r","g","b");rownames(A)=rownames(x);return(A)
+  }
+  else{
+    if(length(x)!=3)stop("need r,g,b!")
+    names(x)=c("r","g","b")
+    if(is.vector(x))return(rgb(x[1],x[2],x[3],maxColorValue = 255))
+    if(is.data.frame(x))return(transmute(x,code=rgb(r,g,b,maxColorValue = 255)))
+    }
 }
 
-#' Plot a multi pages pdf
+
+
+
+#' Plot a multi-pages pdf
 #' @param plist plot list
-#'
 #' @param file prefix of your .pdf file
 #' @param width width
 #' @param height height
@@ -133,16 +171,20 @@ plotpdf<-function(plist,file='new',width=8,height=7,...){
 #' Get n colors
 #'
 #' @param n how many colors you need
-#' @param pal a vector of colors, you can get from here too.{RColorBrewer::brewer.pal(5,"Set2")} {ggsci::pal_aaas()(5)}
+#' @param pal col1~3; or a vector of colors, you can get from here too.{RColorBrewer::brewer.pal(5,"Set2")} {ggsci::pal_aaas()(5)}
+#' @param picture a picture file, colors will be extracted from the picture
 #'
 #' @return n colors
 #' @export
 #'
 #' @examples
-#' get_cols(10)->my_cols
+#' get_cols(10,"col2")->my_cols
 #' scales::show_col(my_cols)
-get_cols <- function (n,pal=NULL){
-  col <- c("#8dd3c7", "#ffffb3", "#bebada", "#fb8072", "#80b1d3",
+#' scales::show_col(get_cols(15,brewer.pal(5,"Set2")))
+#' scales::show_col(get_cols(15,ggsci::pal_aaas()(5)))
+#' scales::show_col(get_cols(4,picture="~/Desktop/Screenshot 2023-02-14 at 16.17.28.png"))
+get_cols <- function (n,pal="col1",picture=NULL){
+  col1 <- c("#8dd3c7", "#ffffb3", "#bebada", "#fb8072", "#80b1d3",
                     "#fdb462", "#b3de69", "#fccde5", "#d9d9d9", "#bc80bd",
                     "#ccebc5", "#ffed6f")
   col2 <- c("#1f78b4", "#ffff33", "#c2a5cf", "#ff7f00", "#810f7c",
@@ -152,9 +194,20 @@ get_cols <- function (n,pal=NULL){
                     "#e31a1c", "#fdbf6f", "#ff7f00", "#cab2d6", "#6a3d9a",
                     "#ffff99", "#b15928")
 
-   if(is.null(pal))pal=col3
-   return(colorRampPalette(pal)(n))
+  if(length(pal)==1)pal=get(pal)
+  if(!is.null(picture)){
+    lib_ps("RImagePalette")
+    type=tools::file_ext(picture)
+    switch (type,
+      "jpg" = {p1=jpeg::readJPEG(picture)},
+      "png" = {p1=png::readPNG(picture)}
+    )
+    pal=RImagePalette::image_palette(p1,n = n)
+  }
+  if(length(pal)<n)return(colorRampPalette(pal)(n))
+  return(pal[seq_len(n)])
 }
+
 
 #' Add a global gg_theme and colors for plots
 #'
@@ -184,7 +237,6 @@ add_theme<-function(set_theme=NULL){
   mytheme<<-mytheme
 }
 
-
 #' @title Plot a boxplot
 #'
 #' @param tab your dataframe
@@ -192,19 +244,24 @@ add_theme<-function(set_theme=NULL){
 #' @param group which colname choose for group or a vector
 #' @param alpha whether plot a group alphabeta
 #' @param method test method:wilcox, tukeyHSD, LSD, (default: wilcox)
-#' @param rain plot a raincloud?
-#' @param p_value show the p-value between each group?(when group number is 2~4)
+#' @param p_value1 multi-test of all group
+#' @param p_value2 two-test of each pair
+#' @param comparisons comparison pairs, as list, e.g. list(c("a","b"))
+#' @param mode 1~3
+#' @param facet facet when only one var?
 #'
 #' @return a 'ggplot' plot object,
 #' @export
 #'
 #' @examples
 #' a=data.frame(a=1:18,b=runif(18,0,5))
-#' group_box(a,group = rep(c("a","b","c"),each=6))
-#' group_box(a[,1,drop=F],group = rep(c("a","b","c"),each=6),alpha=T,mode=3)
+#' group_box(a,group = rep(c("a","b","c"),each=6),p_value1=F,p_value2=T)
+#' multitest(a$a,group = rep(c("a","b","c"),each=6))
+#' group_box(a[,1,drop=F],group = rep(c("a","b","c"),each=6),p_value2=T,mode=3,comparisons=list(c("a","b")))
 #'
-group_box<-function(tab,group=NULL,metadata=NULL,alpha=F,method="wilcox",mode=1,p_value=F){
-  lib_ps("ggplot2","dplyr","ggpubr")
+group_box<-function(tab,group=NULL,metadata=NULL,mode=1,facet=T,
+                    alpha=F,method="wilcox",p_value1=F,p_value2=F,comparisons=NULL,...){
+  lib_ps("ggplot2","dplyr","ggpubr","reshape2")
 #data transform
   g_name=NULL
   if(is.vector(tab))tab=data.frame(value=tab)
@@ -226,7 +283,7 @@ group_box<-function(tab,group=NULL,metadata=NULL,alpha=F,method="wilcox",mode=1,
   }
   md$group<-factor(md$group)
 
-  md%>%melt(id.vars="group",variable.name="indexes")->md
+  md%>%reshape2::melt(id.vars="group",variable.name="indexes")->md
   md$indexes=factor(md$indexes,levels = colnames(tab))
 #main plot
   if(mode==1){p<-ggplot(md,aes(group,value,color=group,group=group))+
@@ -251,19 +308,24 @@ group_box<-function(tab,group=NULL,metadata=NULL,alpha=F,method="wilcox",mode=1,
   }
   p=p+guides(color=guide_legend(g_name),fill=guide_legend(g_name))+
     ylab(label = NULL)+xlab(label = NULL)
+
 #facet?
   flag=(ncol(tab)==1)
-  if(flag) {ylab=colnames(tab)[1];p=p+ylab(ylab)}
-  if(!flag) p=p+facet_wrap(.~indexes,scales = "free_y")
+  if((!flag)|facet) p=p+facet_wrap(.~indexes,scales = "free_y")
+  else {ylab=colnames(tab)[1];p=p+ylab(ylab)}
 
 #p-value?
-  if(p_value){
-    if(between(nlevels(md$group),2,4)){
-    p=p+stat_compare_means(show.legend = FALSE,
-                           comparisons = combn(levels(md$group),2)%>%split(col(.)))
-  }
-    else p=p+stat_compare_means(show.legend = FALSE,label.x = 1)
+  if(p_value1) p=p+stat_compare_means(show.legend = FALSE,label.x = 1,label.y.npc = 0.95)
+  if(p_value2){
+    if(is.null(comparisons)){
+      p=p+stat_compare_means(show.legend = FALSE,
+                            comparisons = combn(levels(md$group),2)%>%split(col(.)),...)
     }
+    else{
+      p=p+stat_compare_means(show.legend = FALSE,
+                             comparisons = comparisons,...)
+    }
+  }
 
   if(alpha){
     a<-list()
@@ -283,7 +345,7 @@ group_box<-function(tab,group=NULL,metadata=NULL,alpha=F,method="wilcox",mode=1,
     }
   }
 
-  if(exists("mytheme"))p=p+mytheme
+  if(exists("mytheme"))if(inherits(mytheme,c("theme","gg")))p=p+mytheme
   return(p)
 }
 
@@ -346,16 +408,15 @@ multitest<-function(var,group,print=T,return=F){
   out1<-agricolae::orderPvalue(levels(group),means,0.05,Q)
   if(return=="wilcox")return(data.frame(out1,variable=rownames(out1)))
   if(print){
-    print("ANOVA:")
+    dabiao("1.ANOVA:")
     print(summary(ano))
-    print('=================================================')
+    dabiao("2.Kruskal.test:")
     print(kruskal.test(var~group))
-    print('=================================================')
-    print('LSDtest分组,bonferroni p-adj:')
+    dabiao("3.LSDtest, bonferroni p-adj:")
     print(lsdres$groups)
-    print('=================================================')
+    dabiao("4.tukeyHSD:")
     print(tukeyres)
-    print('===Wilcox-test====================================')
+    dabiao("5.Wilcox-test:")
     print(Q)
   }
 }
@@ -369,6 +430,7 @@ multitest<-function(var,group,print=T,return=F){
 #'a=runif(50)
 #'fittest(a)
 fittest<-function(a){
+  lib_ps("fitdistrplus","nortest")
   if(is.vector(a)){
     #肉眼看
     plot(a)
@@ -387,10 +449,9 @@ fittest<-function(a){
     #（4）Anderson Darling检验
     nortest::ad.test(a)|>print()
   }
-
 }
 
-#' Plot a donut chart
+#' Plot a doughnut chart
 #'
 #' @param ad a two-columns dataframe, first is type, second is number
 #'
@@ -400,29 +461,38 @@ fittest<-function(a){
 #'
 #' @examples
 #'a=data.frame(type=letters[1:6],num=c(1,3,3,4,5,10))
-#'gghuan(a)
+#'gghuan(a)+scale_fill_manual(values=get_cols(6,"col3"))
 #'b=data.frame(type=letters[1:12],num=c(1,3,3,4,15,10,35,2:6))
-#'gghuan(b)
-
-gghuan<-function(tab,reorder=T,mode="1"){
+#'gghuan(b)+theme(legend.position="right")
+gghuan<-function(tab,reorder=T,mode="1",topN=5,name=T){
   if(ncol(tab)>2)stop("need two columns: first is type, second is number")
   colnames(tab)[1]->g_name
   colnames(tab)<-c("type","n")
   plot_df<-tab%>%group_by(type)%>%
-    summarise(mean=mean(n))%>%mutate(fraction=mean/sum(mean))
+    summarise(mean=mean(n))
 
   if(reorder){
     plot_df$type=reorder(plot_df$type,plot_df$mean)
-    plot_df<-arrange(plot_df,mean)
+    plot_df<-arrange(plot_df,-mean)
   }
+
+  if(nrow(plot_df)>topN){
+    plot_df=rbind(head(plot_df,topN),
+                  data.frame(type="others",
+                             mean=sum(plot_df$mean[(topN+1):nrow(plot_df)])))
+
+    plot_df$type=relevel(factor(plot_df$type),"others")
+  }
+  mutate(plot_df,fraction=mean/sum(mean))->plot_df
+
   plot_df$ymax = cumsum(plot_df$fraction)
   plot_df$ymin = c(0, head(plot_df$ymax, n = -1))
   plot_df$rate_per<-paste(as.character(round(100*plot_df$fraction,1)),'%',sep='')
   if(mode=="1"){plt<-ggplot(data = plot_df, aes(fill = type, ymax = ymax, ymin = ymin, xmax = 3.2, xmin = 1.7)) +
     geom_rect(alpha=0.8) +xlim(c(0, 5)) +
     coord_polar(theta = "y") +
-    geom_text(aes(x = 3.6, y = ((ymin+ymax)/2),label = type) ,size=4)+
     geom_text(aes(x = 2.5, y = ((ymin+ymax)/2),label = rate_per) ,size=3.6,col="white")
+  if(name)plt=plt+geom_text(aes(x = 3.6, y = ((ymin+ymax)/2),label = type) ,size=4)
   }
   if(mode=="2"){plt <- ggplot(plot_df,aes(x = type,y = fraction,fill = type)) +
     geom_col(position = "dodge2",show.legend = TRUE,alpha = .9
@@ -435,13 +505,17 @@ gghuan<-function(tab,reorder=T,mode="1"){
     theme(axis.text=element_blank()) + ## 把图旁边的标签去掉
     theme(axis.ticks=element_blank()) + ## 去掉左上角的坐标刻度线
     theme(panel.border=element_blank(),legend.position = "none")## 去掉最外层的正方形边框
+
 }
 
-#' Fit a lm and plot
+#' Fit a linear model and plot
 #'
 #' @param tab your dataframe
-#' @param metadata the dataframe contains the var
 #' @param var which colname choose for var or a vector
+#' @param metadata the dataframe contains the var
+#' @param facet facet when only one var?
+#' @param ...
+#'
 #' @import ggpmisc
 #' @return a ggplot
 #' @export
@@ -449,7 +523,7 @@ gghuan<-function(tab,reorder=T,mode="1"){
 #' @examples
 #'my_lm(runif(50),var=1:50)
 #'my_lm(c(1:50)+runif(50,0,5),var=1:50)
-my_lm<-function(tab,var,metadata=NULL,...){
+my_lm<-function(tab,var,metadata=NULL,facet=T,...){
   lib_ps("ggplot2","dplyr","ggpubr")
   #data transform
   g_name=NULL
@@ -466,7 +540,7 @@ my_lm<-function(tab,var,metadata=NULL,...){
   }
 
   if(!all(apply(md, 2, is.numeric)))stop("need numeric")
-  md%>%melt(.,id.vars="var",variable.name="indexes")->md
+  md%>%reshape2::melt(.,id.vars="var",variable.name="indexes")->md
   md$indexes=factor(md$indexes,levels = colnames(tab))
   #main plot
   p=ggplot(md,aes(var,value))+
@@ -480,11 +554,12 @@ my_lm<-function(tab,var,metadata=NULL,...){
     labs(x=NULL,y=NULL)
   #facet?
   flag=(ncol(tab)==1)
-  if(flag) {ylab=colnames(tab)[1];p=p+ylab(ylab)}
-  if(!flag) p=p+facet_wrap(.~indexes,scales = "free_y")
+  if((!flag)|facet) p=p+facet_wrap(.~indexes,scales = "free_y")
+  else {ylab=colnames(tab)[1];p=p+ylab(ylab)}
+
   p=p+xlab(g_name)
 
-  if(exists("mytheme"))p=p+mytheme
+  if(exists("mytheme"))if(inherits(mytheme,c("theme","gg")))p=p+mytheme
   return(p)
 }
 
@@ -496,12 +571,13 @@ my_lm<-function(tab,var,metadata=NULL,...){
 #' @export
 #'
 #' @examples
-#'data.frame(row.names = letter[1:18],x=runif(18,30,35),y=runif(18,40,45))->geo
+#'data.frame(row.names = letters[1:18],x=runif(18,30,35),y=runif(18,40,45))->geo
 #'toXY(geo)
-toXY <- function(dat){
+toXY <- function(geo){
   lib_ps("SoDA")
-  XY <- geoXY(dat[,1], dat[,2])
-  return(as.data.frame(row.names = rownames(dat),XY))
+  XY <- geoXY(geo[,1], geo[,2])
+  #geosphere::distm
+  return(as.data.frame(row.names = rownames(geo),XY))
 }
 
 #https://cloud.tencent.com/developer/article/1751856
@@ -552,7 +628,6 @@ china_map<-function(dir="~/Downloads/"){
 #' matrix(letters[1:6],2,3)|>as.data.frame()->a
 #' grepl.data.frame("c",a)
 #' grepl.data.frame("\\w",a)
-
 grepl.data.frame<-function(pattern, x, ...) {
   y <- if (length(x)) {
     do.call("cbind", lapply(x, "grepl", pattern = pattern,...))
@@ -565,3 +640,107 @@ grepl.data.frame<-function(pattern, x, ...) {
   y
 }
 
+#' Read some special format file
+#'
+#' @param file file path
+#' @param format "blast","diamond"
+#'
+#' @return data.frame
+#' @export
+#'
+read.file<-function(file,format=NULL,just_print=F){
+  if(just_print){
+    if(file.size(file)>10000){
+      print(paste0(file,": this file is a little big, still open?"))
+      flag=readline("yes/no(y/n)?")
+      if(tolower(flag)%in%c("yes","y")){
+        cat(readr::read_file(file))
+      }}
+    else cat(readr::read_file(file))
+  }
+  else{
+    if(is.null(format))format=tools::file_ext(file)
+    format=match.arg(format,c("blast","diamond","jpg","png","pdf"))
+
+    if(format%in%c("blast","diamond")){
+      df=read.table(file,sep = "\t",
+                 col.names = c("Qseqid","Sseqid","Pident","Length","Mismatch","Gapopen",
+                               "Qstart","Qend","Sstart","Send","E_value","Bitscore"))
+      return(df)
+    }
+
+    if(format%in%c("jpg","png")){
+      switch (format,
+              "jpg" = {p1=jpeg::readJPEG(file)},
+              "png" = {p1=png::readPNG(file)}
+      )
+      RImagePalette::display_image(p1)
+    }}
+    if(format=="pdf"){
+      lib_ps("pdftools")
+
+    }
+
+}
+
+
+#' @export
+my_cat<-function(mode=1){
+  if(mode==1){  lib_ps("RImagePalette","png")
+  p1=png::readPNG(system.file("data/smallguodong.ppp",package = "pcutils"))
+  RImagePalette::display_image(p1)}
+
+  if(mode==2){  lib_ps("ggimage","ggplot2")
+  t<-seq(0, 2*pi, 0.08)
+  d=data.frame(x=2*(sin(t)-0.5*sin(2*t)),y=2*(cos(t)-0.5*cos(2*t)))
+  ggplot(d, aes(x, y)) +
+    ggimage::geom_image(image = "data/smallguodong.ppp", size = .05)+theme_void()}
+}
+
+
+#' My Sankey plot
+#'
+#' @param test a dataframe with hierarchical structure
+#'
+#' @export
+#'
+#' @examples
+#' data.frame(aa=rep("a",5),a=c("a","a","b","b","c"),b=c("a",LETTERS[2:5]),c=1:5)%>%my_sankey()
+#' data("otutab",package = "pctax")
+#' cbind(taxonomy,num=rowSums(otutab))[1:10,]->test
+#' my_sankey(test)
+my_sankey=function(test){
+  lib_ps("sankeyD3","dplyr")
+  nc=ncol(test)
+  if(nc<3)stop("as least 3-columns dataframe")
+  if(!is.numeric(test[,nc]))stop("the last column must be numeric")
+  #change duplicated data
+  for (i in 1:(nc-2)){
+    for(j in (i+1):(nc-1)){
+      du=intersect(test[,i],test[,j])
+      if(length(du)>0){
+        for (t in du){
+          test[(test[,j]==t),j]=paste0(t,strrep(" ",j-1))
+        }
+      }}
+  }
+  #merge to two columns
+  links=data.frame()
+  for (i in 1:(nc-2)){
+    test[,c(i,i+1,nc)]->tmp
+    colnames(tmp)=c("source","target","weight")
+    tmp=group_by(tmp,source,target)%>%summarise(weight=sum(weight),.groups="keep")
+    links=rbind(links,tmp)
+  }
+  #give ids
+  nodes <- data.frame(name=c(as.character(links$source), as.character(links$target)) %>% unique())
+  links$IDsource <- match(links$source, nodes$name)-1
+  links$IDtarget <- match(links$target, nodes$name)-1
+
+  sankeyNetwork(Links = as.data.frame(links), Nodes = nodes, Source = "IDsource", Target = "IDtarget",
+                Value = "weight", NodeID = "name",nodeWidth =10,units = 'TWh',
+                height=400,width=300,
+                colourScale=JS("d3.scaleOrdinal(d3.schemeCategory10);"),
+                #numberFormat=".0f",
+                fontSize = 8)
+}
