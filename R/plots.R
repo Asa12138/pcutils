2cfd9ddfee776f27a8c66c3799274592a1dc6a43
Asa12138 <3180105399@zju.edu.cn>
2025-03-27 13:37:23 +0800

    fixed gghuan2 example

--- R/plots.R
+++ R/plots.R
@@ -1558,14 +1558,15 @@ gghuan <- function(tab, reorder = TRUE, mode = "1", topN = 5, name = TRUE, perce
 #' @examples
 #' \donttest{
 #' if (interactive()) {
-#' data.frame(
-#'   a = c("a", "a", "b", "b", "c"), b = c("a", LETTERS[2:5]), c = rep("a", 5),
-#'   number = 1:5
-#' ) %>% gghuan2()
+#'   data.frame(
+#'     a = c("a", "a", "b", "b", "c"), b = c("a", LETTERS[2:5]), c = rep("a", 5),
+#'     number = 1:5
+#'   ) %>% gghuan2()
 #' }
 #' }
 gghuan2 <- function(tab = NULL, huan_width = 1, circle_width = 1, space_width = 0.2, circle_label = NULL, pal = NULL,
                     name = TRUE, percentage = FALSE, text_params = NULL, circle_label_params = NULL, bar_params = NULL) {
+  lib_ps("ggnewscale", library = FALSE)
   if (!is.numeric(tab[, ncol(tab)])) stop("the last column must be numeric")
   if ((space_width < 0) | space_width >= 1) stop("space_width should be [0,1)")
   type <- ymax <- ymin <- xmin <- xmax <- lab <- fraction <- NULL
